a9df2eed84e7a5806cc07c01c79f02de40cbf11e
DavidHrrsn <davidhrrsn8@gmail.com>
2026-06-07 18:17:31 -0700

    Added JavaDoc comments

--- src/sunrisesunsetupdated/Main.java
+++ src/sunrisesunsetupdated/Main.java
@@ -1,9 +1,30 @@
+/**
+ * Main.java
+ *
+ * Entry point for the Sunrise/Sunset Time Finder application.
+ * This class delegates application startup to the JavaFX
+ * SunriseSunsetApp class.
+ *
+ * CMSC 495 Capstone Project
+ * Project: Sunrise/Sunset Time Finder
+ *
+ * @author David Harrison
+ * @author Samuel Garmoe
+ * @author Apurva Dave
+ * @author Jeremy Ross
+ * @author Jeremy Briggs
+ * @version 1.0
+ */
 package sunrisesunsetupdated;
 
 public class Main {
 
+    /**
+     * Launches the JavaFX application.
+     *
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args) {
-
         SunriseSunsetApp.main(args);
     }
 }
