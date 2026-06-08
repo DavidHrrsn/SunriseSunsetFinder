d9c03f128abf9a9e9077d70585ab36821df1b88b
DavidHrrsn <davidhrrsn8@gmail.com>
2026-06-07 18:18:13 -0700

    Added JavaDoc comments

--- src/sunrisesunsetupdated/LocationResult.java
+++ src/sunrisesunsetupdated/LocationResult.java
@@ -1,23 +1,47 @@
 package sunrisesunsetupdated;
-/*
- * LocationResult
- * --------------
- * Stores the latitude, longitude, and display name returned
- * from the geocoding service.
+
+/**
+ * LocationResult.java
+ *
+ * Stores location information returned by the geocoding service,
+ * including latitude, longitude, and a formatted display name.
+ *
+ * This class acts as a simple data container that transfers
+ * geographic location information between application components.
+ *
+ * CMSC 495 Capstone Project
+ * Project: Sunrise/Sunset Time Finder
+ *
+ * @author David Harrison
+ * @author Samuel Garmoe
+ * @author Apurva Dave
+ * @author Jeremy Ross
+ * @author Jeremy Briggs
+ * @version 1.0
  */
 public class LocationResult {
 
-    // Latitude returned from geocoding API
+    /**
+     * Latitude returned by the geocoding service.
+     */
     private final double latitude;
 
-    // Longitude returned from geocoding API
+    /**
+     * Longitude returned by the geocoding service.
+     */
     private final double longitude;
 
-    // Full formatted address/location name
+    /**
+     * Formatted location name returned by the geocoding service.
+     */
     private final String displayName;
 
-    /*
-     * Constructor
+    /**
+     * Constructs a LocationResult object containing location data.
+     *
+     * @param latitude Geographic latitude of the location.
+     * @param longitude Geographic longitude of the location.
+     * @param displayName Formatted location name.
      */
     public LocationResult(
             double latitude,
@@ -30,29 +54,38 @@ public class LocationResult {
         this.displayName = displayName;
     }
 
-    /*
-     * Returns latitude
+    /**
+     * Returns the latitude associated with the location.
+     *
+     * @return Geographic latitude.
      */
     public double getLatitude() {
         return latitude;
     }
 
-    /*
-     * Returns longitude
+    /**
+     * Returns the longitude associated with the location.
+     *
+     * @return Geographic longitude.
      */
     public double getLongitude() {
         return longitude;
     }
 
-    /*
-     * Returns display name
+    /**
+     * Returns the formatted display name of the location.
+     *
+     * @return Formatted location name.
      */
     public String getDisplayName() {
         return displayName;
     }
 
-    /*
-     * Returns formatted location information
+    /**
+     * Returns a formatted string representation of the location.
+     *
+     * @return Location information including display name,
+     *         latitude, and longitude.
      */
     @Override
     public String toString() {
